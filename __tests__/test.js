ce98c2f3eb9514897555116ca24b1d7932e0760b
safer <asafron2@ford.com>
2019-06-06 12:07:04 +0300

    add test

---
+++ __tests__/test.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+
+const parse = require('../src');
+
+it('should parse form with fields', () => {
+  const bodyLines = [
+    '--boundary',
+    'Content-Disposition: form-data; name="name"',
+    '',
+    'Jon Snow',
+    '--boundary',
+    'Content-Disposition: form-data; name="age"',
+    '',
+    '23',
+    '--boundary--'
+  ];
+  const request = {
+    headers: {
+      'Content-Type': 'multipart/form-data; boundary=boundary'
+    },
+    body: Buffer.from(bodyLines.join('\r\n'))
+  }
+
+  const { fields } = parse(request);
+  expect(fields).toEqual({ name: 'Jon Snow', age: '23' });
+});
+
+it('should parse form with files', () => {
+  const fileBuffer = fs.readFileSync(path.join(__dirname, './_fixtures/test.xlsx'));
+  const bodyLines = [
+    '--boundary',
+    'Content-Disposition: form-data; name="file"; filename="test.xlsx"',
+    '',
+    `${fileBuffer.toString('binary')}`,
+    '--boundary--'
+  ];
+  const request = {
+    headers: {
+      'Content-Type': 'multipart/form-data; boundary=boundary'
+    },
+    body: Buffer.from(bodyLines.join('\r\n'))
+  }
+
+  const { files } = parse(request);
+  const { file } = files;
+  expect(file.filename).toEqual('test.xlsx');
+  expect(Buffer.compare(fileBuffer, file.contenr)).toEqual(0);
+});
