9949f44a38b2a1907b46d6cfdb1c1be091f02103
safer-bwd <35757462+safer-bwd@users.noreply.github.com>
2019-06-06 21:12:29 +0300

    remove cyrillic symbols

--- src/utils/parse-header-options.js
+++ src/utils/parse-header-options.js
@@ -1,4 +1,4 @@
 export default str => str.split(';')
   .map(item => item.split('=').map(i => i.trim().replace(/^"(.*)"$/, '$1')))
   .filter(item => item.length > 1)
-  .reduce((aсс, [key, val]) => ({ ...aсс, [key]: val }), {});
+  .reduce((acc, [key, val]) => ({ ...acc, [key]: val }), {});
