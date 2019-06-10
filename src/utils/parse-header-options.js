b0e458828432aa1fb8cea204c2cb0dc8507f4942
safer <asafron2@ford.com>
2019-06-10 08:56:56 +0300

    refactor (parse-header-options)

--- src/utils/parse-header-options.js
+++ src/utils/parse-header-options.js
@@ -1,4 +1,6 @@
+const removeQuotes = str => str.replace(/^"(.*)"$/, '$1');
+
 export default str => str.split(';')
   .map(item => item.split('=').map(i => i.trim()))
   .filter(item => item.length > 1)
-  .reduce((acc, [key, val]) => ({ ...acc, [key]: val.replace(/^"(.*)"$/, '$1') }), {});
+  .reduce((acc, [key, val]) => ({ ...acc, [key]: removeQuotes(val) }), {});
